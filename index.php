8f8dfa26ff940fbdc60d7b94ca5abd80394cd38a
Engineer Kenneth <kennethusiobaifo@yahoo.com>
2016-12-26 18:08:35 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta charset="UTF-8">
+<meta name="description" content="Topclasssreporters offficial website">
+<meta name="author" content="Engineer Usiobaifo Airuleyemwangbon Kenneth, programmer">
+<link href="css/main.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+This is the home page
+</body>
+</html>
